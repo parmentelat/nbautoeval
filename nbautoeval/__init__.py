e04dcc3c4b8841cb025251455895e2451a506104
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-07-25 18:33:33 +0200

    expose symbols globally

---
+++ nbautoeval/__init__.py
@@ -0,0 +1,4 @@
+from .args import Args
+from .exercise_function import ExerciseFunction
+from .exercise_regexp import ExerciseRegexp, ExerciseRegexpGroups
+from .exercise_class import ExerciseClass, ScenarioClass
