ea3efbfe70ea1796add6100ffc562e4ba27dcfda
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-10-11 12:23:37 +0200

    updated documentation on class-based exercises

--- exercises/fifoclass.py
+++ exercises/fifoclass.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-
-from nbautoeval.exercise_class import ExerciseClass, ScenarioClass
-from nbautoeval.args import Args
+# # provide a workable class
 
 ####################
-class Fifo(object):
+class Fifo:
 
     def __init__(self):
         self.items = []
 
     def __repr__(self):
-        return "<Fifo [{}]>".format(" ".join(["{}".format(i) for i in self.items]))
+        contents = join(["{}".format(i) for i in self.items])
+        return f"<Fifo [{contents}]>"
 
     def incoming(self, incoming):
         self.items.append(incoming)
@@ -19,30 +18,43 @@ class Fifo(object):
         if len(self.items) == 0:
             return None
         return self.items.pop(0)
+    
+    def __len__(self):
+        return len(self.items)
 
 
+# # create an exercise instance
 
-scenario1 = ScenarioClass()
-scenario1.set_init_args( Args() )
-scenario1.add_step( 'incoming', Args(1) )
-scenario1.add_step( 'incoming', Args(2) )
-scenario1.add_step( 'outgoing', Args() )
-scenario1.add_step( 'incoming', Args(3) )
-scenario1.add_step( 'incoming', Args(4) )
-scenario1.add_step( 'outgoing', Args() )
+from nbautoeval.exercise_class import ExerciseClass, ScenarioClass
+from nbautoeval.args import Args
 
-scenario2 = ScenarioClass()
-scenario2.set_init_args(Args ())
-scenario1.add_step( 'outgoing', Args() )
-scenario2.add_step( 'incoming', Args(1) )
-scenario2.add_step( 'incoming', Args(2) )
-scenario2.add_step( 'incoming', Args(3) )
-scenario2.add_step( 'incoming', Args(4) )
-scenario2.add_step( 'outgoing', Args() )
+scenario1 = ScenarioClass(
+    # arguments to the constructor
+    Args(),
+    # a list of expressions, with 
+    # INSTANCE and CLASS replaced as appropriate
+    "INSTANCE.incoming(1)",
+    "INSTANCE.incoming(2)",
+    "INSTANCE.outgoing()",
+    "INSTANCE.incoming(3)",
+    "INSTANCE.incoming(4)",
+    "INSTANCE.outgoing()",
+    "len(INSTANCE)"
+)
+
+scenario2 = ScenarioClass(
+    Args(),
+    "INSTANCE.outgoing()",
+    "INSTANCE.incoming(1)",
+    "INSTANCE.incoming(2)",
+    "INSTANCE.incoming(3)",
+    "INSTANCE.incoming(4)",
+    "INSTANCE.outgoing()",
+)    
 
 exo_fifo = ExerciseClass (Fifo, [scenario1, scenario2],
                           layout='pprint')
 
 if __name__ == '__main__':
     exo_fifo.correction(Fifo)
-                          
+
