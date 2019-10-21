5e9afd96d8d2a52c48fbf867d6d1a0631f7a6b92
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-10-21 18:48:25 +0200

    expose __version__ in package

--- nbautoeval/__init__.py
+++ nbautoeval/__init__.py
@@ -3,3 +3,5 @@ from .exercise_function import ExerciseFunction
 from .exercise_generator import ExerciseGenerator
 from .exercise_regexp import ExerciseRegexp, ExerciseRegexpGroups
 from .exercise_class import ExerciseClass, ClassScenario, ClassExpression, ClassStatement
+
+from .version import __version__
